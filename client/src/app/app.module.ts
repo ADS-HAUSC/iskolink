5dc82e440252c16f0334103bc86f1079dd3fab30
Bancantcode <santiagobryan831@gmail.com>
2025-03-12 20:10:25 +0800

    added routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// import { AppRoutingModule } from './app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+// import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { ContactComponent } from './contact/contact.component';
+import { ScholarshipComponent } from './scholarship/scholarship.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ActivitiesComponent,
+    ContactComponent,
+    ScholarshipComponent,
+    AdminLoginComponent,
+    AdminDashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule
-    HttpClientModule
+    AppRoutingModule,
+    // HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/scholarship/scholarship.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scholarship',
+  standalone: false,
+  templateUrl: './scholarship.component.html',
+  styleUrl: './scholarship.component.css'
+})
+export class ScholarshipComponent {
+
+}
