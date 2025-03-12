190c729d8e316e44ff16be28f3cf852bbf67be98
Bancantcode <santiagobryan831@gmail.com>
2025-03-12 21:05:48 +0800

    added guides in all .html pages | added constants for backend purpose

---
+++ client/src/constants/constants.ts
@@ -0,0 +1,26 @@
+export const activities = [
+    {
+        title: "",
+        img1: "",
+        img2: "",
+        img3: "",
+        desc1: "",
+        desc2: "",
+    },
+    {
+        title: "",
+        img1: "",
+        img2: "",
+        img3: "",
+        desc1: "",
+        desc2: "",
+    },
+    {
+        title: "",
+        img1: "",
+        img2: "",
+        img3: "",
+        desc1: "",
+        desc2: "",
+    },
+]
